76e0442e1beb9c55a7ab0ddfc6b08db5447e85a2
Matthew Dolan <matthew.dolan@ballysinteractive.com>
2022-10-29 07:09:51 -0700

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Matthew Dolan on 2022-10-29.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
