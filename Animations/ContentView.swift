0b38923d45382c1b3060fa2f9fedf82cfe40a93a
Matthew Dolan <matthew.dolan@ballysinteractive.com>
2022-10-31 06:51:01 -0700

    Day 32: Project 6, part 1 - Animating bindings

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,33 +11,25 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Tap me") {
-//            animationAmount += 1
+        print(animationAmount)
+        
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeInOut(duration: 1)
-                        .repeatForever (autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
-        }
-//        .scaleEffect(animationAmount)
-//        .blur(radius: (animationAmount - 1) * 3)
-//        .animation(
-//            .easeInOut(duration: 2)
-//                .repeatForever (autoreverses: true),
-//            value: animationAmount)
     }
 }
 
