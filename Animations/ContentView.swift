da09ab50d1c41f9bb0553334fd087ae20f70f06e
Matthew Dolan <matthew.dolan@ballysinteractive.com>
2022-11-02 06:51:44 -0700

    Day 33: Project 6, part 2 - Showing and hiding views with transitions

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,61 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    // 2.
-    let letters = Array("Hello, SwiftUI")
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
-    
-    // 1.
-//    @State private var dragAmount = CGSize.zero
+    @State private var isShowingRed = false
     
     var body: some View {
-        // 2.
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? .blue : .red)
-                    .offset(dragAmount)
-                    .animation(
-                        .default.delay(Double(num) / 20),
-                        value: dragAmount)
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                // 2.
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                // 1.
+//                    .transition(.scale)
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged { dragAmount = $0.translation }
-                .onEnded { _ in
-                    dragAmount = .zero
-                    enabled.toggle()
-                }
-        )
-        
-        
-        
-        
-        
-        
-        
-        // 1.
-//        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
-//            .frame(width: 300, height: 200)
-//            .clipShape(RoundedRectangle(cornerRadius: 10))
-//            .offset(dragAmount)
-//            .gesture(
-//                DragGesture()
-//                    .onChanged{ dragAmount = $0.translation }
-//                    .onEnded{ _ in
-//                        // 1b.
-//                        withAnimation {
-//                            dragAmount = .zero
-//
-//                        }
-//                    }
-//            )
-        // 1a.
-//            .animation(.spring(), value: dragAmount)
     }
 }
     
